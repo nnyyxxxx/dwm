9ee53024cec6cbd171cf9c9996af3d257ecbe2fc
Nyx <144965845+nnyyxxxx@users.noreply.github.com>
2024-10-02 21:13:24 -0400

    Revert "Blocks (#60)"
    This reverts commit d343a22c768694d2cbbb5eea9dc6bd3278044612.

--- suckless/dwmblocks/config.h
+++
@@ -1,9 +0,0 @@
-static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"\x07  \x01", "date '+%I:%M %p'",					60,		0},
-	{"\x08  \x01", "checkupdates | wc -l",				60,		0},
-	{"\x09  \x01", "pamixer --get-volume-human",		60,		0},
-};
-
-static char delim[] = "  ";
-static unsigned int delimLen = 5;
